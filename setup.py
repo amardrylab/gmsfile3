df841b4c2638286de27467a989a36749c9ec3738
Gourab Kanti Das <gourabkanti.das@visva-bharati.ac.in>
2019-09-12 09:27:38 +0000

    preparing for python3

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
         name="gmsfile",
-        version="0.0.6",
+        version="0.1.0",
         author="Gourab Kanti Das",
         author_email="gourabkanti.das@visva-bharati.ac.in",
         description="A converter from XYZ to GAMESS file",
         long_description=long_description,
         long_description_content_type="text/markdown",
-        url="https://github.com/amardrylab/gmsfile",
+        url="https://github.com/amardrylab/gmsfile3",
         packages = setuptools.find_packages(),
         classifiers=[
-            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3.7",
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
         ],
-        python_requires='>=3.4',
+        python_requires='>=3.7',
 )
